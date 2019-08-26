c49b642f57b07718ef989f0d86980f67eeaa5769
Jan Skowronski <crianonim@gmail.com>
2019-08-26 08:03:52 +0100

    Add status parameter

--- echohandler.go
+++ echohandler.go
@@ -9,9 +9,12 @@ import (
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
 	resObj := response{Method: r.Method, Url: r.URL.String(), Query: r.URL.RawQuery, QueryList: r.URL.Query(), Header: r.Header}
-	// fmt.Println(resObj, r.URL.Query(), r.Header)
 	if delay, _ := strconv.Atoi(r.URL.Query().Get("delay")); delay > 0 {
 		time.Sleep(time.Duration(delay) * time.Second)
+	}
+	if status, _ := strconv.Atoi(r.URL.Query().Get("status")); status > 0 {
+		w.WriteHeader(status)
+		return
 	}
 	s, e := json.Marshal(resObj)
 	if e != nil {
