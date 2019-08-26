53014ced538ddab33e17921583a701d4d0e313c6
Jan Skowronski <crianonim@gmail.com>
2019-08-26 07:50:59 +0100

    Add delay parameter

--- echohandler.go
+++ echohandler.go
@@ -3,16 +3,20 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
 	resObj := response{Method: r.Method, Url: r.URL.String(), Query: r.URL.RawQuery, QueryList: r.URL.Query(), Header: r.Header}
 	// fmt.Println(resObj, r.URL.Query(), r.Header)
+	if delay, _ := strconv.Atoi(r.URL.Query().Get("delay")); delay > 0 {
+		time.Sleep(time.Duration(delay) * time.Second)
+	}
 	s, e := json.Marshal(resObj)
 	if e != nil {
 		w.WriteHeader(500)
 		w.Write([]byte("Bad json"))
-
 	}
 	w.Write(s)
 
